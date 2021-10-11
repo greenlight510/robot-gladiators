1694e24358e9853a9b11291e28c5b7d6ce7ae754
greenlight510 <90018321+greenlight510@users.noreply.github.com>
2021-10-11 16:55:20 -0700

    point scoring

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,14 +10,37 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function() {
-  window.alert("Welcome to Robot Gladiators!");
+window.alert("Welcome to Robot Gladiators!");
 
-  //Keep Enemyhealth Points
-    enemyHealth = (enemyHealth - playerAttack);
-    console.log(  playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-    );
-    playerHealth = (playerHealth - enemyAttack);
-    console.log( enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-      );
+//Keep Enemyhealth Points
+enemyHealth = (enemyHealth - playerAttack);
+console.log(  playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+);
+
+//Enemy Health Check
+if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died ")
+}
+
+else {
+    window.alert(enemyName + " still has " + " health left.")
+}
+
+//Player Health Check
+if (playerHealth <= 0) {
+    window.alert(playerName + " has died")
+}
+
+else {
+    window.alert(playerName + " still has " + " health left.")
+}
+playerHealth = (playerHealth - enemyAttack);
+console.log( enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+);
+if (playerHealth > 0) {
+console.log("Your player is still alive!");
 };
+  
+};
+
   fight();
