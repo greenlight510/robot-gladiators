5b9837139b5c7f5542fe22b1019e21984239105b
greenlight510 <90018321+greenlight510@users.noreply.github.com>
2021-10-11 15:09:05 -0700

    added js code blocks

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,13 @@
-window.prompt("What is your robot's name?");
-
-
-
-
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
 
 // this creates a function named "fight"
 
 function fight() {
     window.alert("The fight has begun!");
   }
-  fight();
+  //fight();
